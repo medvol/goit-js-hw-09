a2091a4d8574ad0104b4a671ae1439755f33bdb1
Oleh Medvid <olmedvid@gmail.com>
2022-07-26 17:55:29 +0300

    add: resolve Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.startBtn.classList.add('btn', 'btn--play');
+refs.stopBtn.classList.add('btn', 'btn--stop');
+
+refs.startBtn.addEventListener('click', handleStartBtn);
+refs.stopBtn.addEventListener('click', handleStopBtn);
+
+function handleStartBtn() {
+    if (refs.startBtn.hasAttribute('disabled')) {
+        return;
+    }
+
+    timerID = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+        refs.startBtn.setAttribute('disabled', true)
+        
+    }, 1000);
+    
+};
+
+function handleStopBtn() {
+    refs.startBtn.removeAttribute('disabled');
+    clearInterval(timerID);
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
