46656107c708aaa2b81a64cdfb676943ee45ef11
Oleh Medvid <olmedvid@gmail.com>
2022-07-28 17:29:18 +0300

    add: Task2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,50 +1,68 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-const startBtn = document.querySelector('[data-start]');
-const counterDays = document.querySelector('[data-days]');
-const counterHours = document.querySelector('[data-hours]');
-const counterMinutes = document.querySelector('[data-minutes]');
-const counterSeconds = document.querySelector('[data-seconds]');
+Notify.init({position: 'center-top'})
 
-startBtn.setAttribute('disabled', true)
+const refs = {
+startBtn: document.querySelector('[data-start]'),
+counterDays: document.querySelector('[data-days]'),
+counterHours: document.querySelector('[data-hours]'),
+counterMinutes: document.querySelector('[data-minutes]'),
+counterSeconds: document.querySelector('[data-seconds]'),
+}
+
+refs.startBtn.setAttribute('disabled', true)
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-        if (selectedDates[0] < new Date()) {
-            window.alert("Please choose a date in the future")
-            return;
-      }
-        startBtn.removeAttribute('disabled');
-        const deadlineTime = selectedDates[0];
-        // return deadlineTime;
+  onClose(selectedDates) {
+     
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Please choose a date in the future');
+        // window.alert("Please choose a date in the future")
+        return;
+  }
+    refs.startBtn.removeAttribute('disabled');
+    Notify.success('Please, press to Start to activate countdown');
+    
   },
 };
 
-
-
 const calendar = flatpickr("#datetime-picker", options);
 
-// console.log(calendar.selectedDates)
-
-const deadlineTime = calendar.onClose;
-
-console.log(deadlineTime)
-
-
-startBtn.addEventListener('click', handleStartBtn);
+refs.startBtn.addEventListener('click', handleStartBtn);
 
 function handleStartBtn() {
-    const currentTime = new Date();
+  let isActiveTimer = false;
+  let intervalID = null;
+  if (isActiveTimer)  return ;
+
+  isActiveTimer = true;
+  const deadlineTime = calendar.selectedDates[0];
+  
+// refs.startBtn.setAttribute('disabled', true) або зробити неактивною кнопку коли таймер вже запущено
+
+  intervalID = setInterval(() => {
+    
+    const currentTime = Date.now();
     const deltaTime = deadlineTime - currentTime;
-    console.log(deltaTime)
+    const convertTime = convertMs(deltaTime);
+    updateTimerMarkup(convertTime);
+    
+    if (deltaTime <= 0) {
+     
+      clearInterval(intervalID);
+      updateTimerMarkup({ days:0,hours:0,minutes:0,seconds:0});
+      isActiveTimer = false;
+    }
+  }, 1000);
     
 }
-
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -63,4 +81,19 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
+
+function addLeadingZero(value) {
+
+  return String(value).padStart(2, '0');
+   
+};
+
+function updateTimerMarkup({days, hours, minutes, seconds}) {
+
+  refs.counterDays.textContent = addLeadingZero(days);
+  refs.counterHours.textContent = addLeadingZero(hours);
+  refs.counterMinutes.textContent = addLeadingZero(minutes);
+  refs.counterSeconds.textContent = addLeadingZero(seconds);
+  
+};
